3ab1d3b0ced04d3fdae06e4aa2d469d91e58715e
Vitalii Chura <vcvitaly@yahoo.com>
2024-10-10 13:44:24 +0300

    Initial commit

---
+++ src/main/java/io/github/vcvitaly/solidgate/task/SolidgateTaskApplication.java
@@ -0,0 +1,13 @@
+package io.github.vcvitaly.solidgate.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidgateTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidgateTaskApplication.class, args);
+	}
+
+}
