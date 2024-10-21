b99c538039e7b25f85048b00eef7deb0ef51aa52
Vitalii Chura <vcvitaly@yahoo.com>
2024-10-21 18:22:05 +0300

    draft an int test

---
+++ src/test/java/io/github/vcvitaly/solidgate/task/SolidgateTaskIntegrationTest.java
@@ -0,0 +1,28 @@
+package io.github.vcvitaly.solidgate.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+class SolidgateTaskIntegrationTest {
+
+	@Container
+	private static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:16");
+
+	@Test
+	void contextLoads() {
+	}
+
+	@DynamicPropertySource
+	static void registerProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+		registry.add("spring.datasource.username", POSTGRES::getUsername);
+		registry.add("spring.datasource.password", POSTGRES::getPassword);
+	}
+}
